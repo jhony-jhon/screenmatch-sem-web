a0beefb087af8568601de12a79f2dc944fc8779a
Jhony Batista <telinabatista@gmail.com>
2024-04-29 19:33:14 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Gilmore+Girls&apikey=f11e6694");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
